d0a3780a5db5328322b84cfe0eb3747e1aa7e15b
VictorienXP <VictorienXP@Xperidia.com>
2020-06-02 20:34:33 +0200

    Update init.lua

--- gamemodes/supercookingpanic/gamemode/init.lua
+++ gamemodes/supercookingpanic/gamemode/init.lua
@@ -11,8 +11,40 @@ include("shared.lua")
 include("player.lua")
 
 
+--[[---------------------------------------------------------
+	Name: gamemode:Initialize()
+	Desc: Called immediately after starting the gamemode
+-----------------------------------------------------------]]
 function GM:Initialize()
 
 	self:SharedInitialize()
 
+end
+
+--[[---------------------------------------------------------
+	Name: gamemode:InitPostEntity()
+	Desc: Called as soon as all map entities have been spawned
+-----------------------------------------------------------]]
+function GM:InitPostEntity()
+end
+
+--[[---------------------------------------------------------
+	Name: gamemode:Think()
+	Desc: Called every frame
+-----------------------------------------------------------]]
+function GM:Think()
+end
+
+--[[---------------------------------------------------------
+	Name: gamemode:ShutDown()
+	Desc: Called when the Lua system is about to shut down
+-----------------------------------------------------------]]
+function GM:ShutDown()
+end
+
+--[[---------------------------------------------------------
+	Name: gamemode:EntityTakeDamage( target, dmg )
+	Desc: The entity has received damage
+-----------------------------------------------------------]]
+function GM:EntityTakeDamage(target, dmg)
 end
