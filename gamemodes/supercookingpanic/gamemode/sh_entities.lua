ce9eb3253d6e7c36acaecbbd0204329bbf04887a
VictorienXP <VictorienXP@Xperidia.com>
2020-06-18 17:25:51 +0200

    Add prop_physics_respawnable

--- gamemodes/supercookingpanic/gamemode/sh_entities.lua
+++ gamemodes/supercookingpanic/gamemode/sh_entities.lua
@@ -7,6 +7,7 @@
 local ingredient_class = {
 	["prop_physics"] = true,
 	["prop_physics_multiplayer"] = true,
+	["prop_physics_respawnable"] = true,
 	["prop_ragdoll"] = true,
 	["player"] = true,
 	["npc_citizen"] = true,
