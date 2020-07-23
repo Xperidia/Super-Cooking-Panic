ef9f7ad53d14c3823d5d9ee4946c44096c25aa35
VictorienXP <VictorienXP@Xperidia.com>
2020-07-23 12:07:46 +0200

    Create info_cooking_pot

---
+++ gamemodes/supercookingpanic/entities/entities/info_cooking_pot.lua
@@ -0,0 +1,11 @@
+--[[---------------------------------------------------------
+		Super Cooking Panic for Garry's Mod
+				by Xperidia (2020)
+-----------------------------------------------------------]]
+
+AddCSLuaFile()
+
+ENT.Base = "base_entity"
+ENT.Type = "point"
+ENT.PrintName = "Spawn point for the cooking pots"
+ENT.Author = "Xperidia"
