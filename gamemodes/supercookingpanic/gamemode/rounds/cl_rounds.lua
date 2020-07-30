9cdd5db858a003276c44cdfe33af8bbcd3b167da
VictorienXP <VictorienXP@Xperidia.com>
2020-07-30 20:48:23 +0200

    Revert "Add round state change sounds"
    This reverts commit 6e6db3609c946ba44aebc1a815e027a85cf70ce8.

--- gamemodes/supercookingpanic/gamemode/rounds/cl_rounds.lua
+++ gamemodes/supercookingpanic/gamemode/rounds/cl_rounds.lua
@@ -10,14 +10,4 @@ net.Receive("scookp_roundupdate", function(len)
 	GAMEMODE.RoundVars.state = net.ReadUInt(32)
 	GAMEMODE.RoundVars.timer = net.ReadFloat()
 
-	if GAMEMODE.RoundVars.state == RND_PLAYING then
-
-		surface.PlaySound("supercookingpanic/effects/time_up.wav")
-
-	elseif GAMEMODE.RoundVars.state == RND_ENDING then
-
-		surface.PlaySound("supercookingpanic/effects/time_running_up.wav")
-
-	end
-
 end)
