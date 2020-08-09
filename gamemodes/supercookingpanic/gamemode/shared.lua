28dbe20f33c8c9ed8894be2c4cc4585c34d50e8b
VictorienXP <VictorienXP@Xperidia.com>
2020-08-09 23:30:07 +0200

    Update shared.lua

--- gamemodes/supercookingpanic/gamemode/shared.lua
+++ gamemodes/supercookingpanic/gamemode/shared.lua
@@ -9,7 +9,7 @@ GM.Name 			= "Super Cooking Panic"
 GM.Prefix			= "scookp"
 GM.Author 			= "Xperidia"
 GM.Website 			= "steamcommunity.com/sharedfiles/filedetails/?id=2180715133"
-GM.Version 			= "v0.7.0"
+GM.Version 			= "v0.7.1"
 GM.VersionDate 		= 200803
 GM.TeamBased 		= true
 GM.AllowAutoTeam	= true
