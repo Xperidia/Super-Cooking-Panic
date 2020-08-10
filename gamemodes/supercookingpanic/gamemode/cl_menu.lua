db6a5b50cbdd68d9ebc6a2ad83e5b441dab5d1a7
VictorienXP <VictorienXP@Xperidia.com>
2020-08-10 16:39:21 +0200

    Update gamemode menu

--- gamemodes/supercookingpanic/gamemode/cl_menu.lua
+++ gamemodes/supercookingpanic/gamemode/cl_menu.lua
@@ -5,12 +5,13 @@
 
 local function binds()
 	return {
-		{GAMEMODE:CheckBind("gm_showhelp"), "Open Help", "Gamemode menu"},
-		{GAMEMODE:CheckBind("gm_showteam"), "Open Team Menu", "Change team"},
-		{GAMEMODE:CheckBind("+attack"), language.GetPhrase("#Valve_Primary_Attack"), "Grab ingredient"},
-		{GAMEMODE:CheckBind("+attack2"), language.GetPhrase("#Valve_Secondary_Attack"), "Use Power-UP"},
-		{GAMEMODE:CheckBind("+reload"), language.GetPhrase("#Valve_Reload_Weapon"), "Drop ingredient"},
-		{GAMEMODE:CheckBind("gmod_undo"), "Undo", "Drop Power-UP"},
+		{GAMEMODE:CheckBind("gm_showhelp"), "Open Help", "#scookp_menu_bind_gm_showhelp"},
+		{GAMEMODE:CheckBind("gm_showteam"), "Open Team Menu", "#scookp_menu_change_team"},
+		{GAMEMODE:CheckBind("+attack"), language.GetPhrase("#Valve_Primary_Attack"), "#scookp_menu_bind_grab"},
+		{GAMEMODE:CheckBind("+attack2"), language.GetPhrase("#Valve_Secondary_Attack"), "#scookp_menu_bind_power_up"},
+		{GAMEMODE:CheckBind("+reload"), language.GetPhrase("#Valve_Reload_Weapon"), "#scookp_menu_bind_drop"},
+		{GAMEMODE:CheckBind("gmod_undo"), "Undo", "#scookp_menu_bind_drop_power_up"},
+		{GAMEMODE:CheckBind("+use"), language.GetPhrase("#Valve_Use_Items")},
 		{GAMEMODE:CheckBind("+forward"), language.GetPhrase("#Valve_Move_Forward")},
 		{GAMEMODE:CheckBind("+moveleft"), language.GetPhrase("#Valve_Move_Left")},
 		{GAMEMODE:CheckBind("+moveright"), language.GetPhrase("#Valve_Move_Right")},
@@ -18,14 +19,19 @@ local function binds()
 		{GAMEMODE:CheckBind("+speed"), language.GetPhrase("#Valve_Sprint")},
 		{GAMEMODE:CheckBind("+jump"), language.GetPhrase("#Valve_Jump")},
 		{GAMEMODE:CheckBind("+duck"), language.GetPhrase("#Valve_Duck")},
-		{GAMEMODE:CheckBind("+use"), language.GetPhrase("#Valve_Use_Items")},
 	}
 end
 
-local function do_tab(name, icon, parent)
+local function do_tab(name, icon, parent, noscroll)
 
 	local DPanel = vgui.Create("DPanel", parent)
 	parent:AddSheet(name, DPanel, icon)
+
+	if noscroll then
+
+		return DPanel
+
+	end
 
 	local DScrollPanel = vgui.Create("DScrollPanel", DPanel)
 	DScrollPanel:Dock(FILL)
@@ -42,6 +48,15 @@ local function do_text(str, parent)
 	DLabel:SetDark(1)
 
 	return DLabel
+
+end
+
+local function do_rich_text(parent)
+
+	local richtext = vgui.Create("RichText", parent)
+	richtext:Dock(FILL)
+
+	return richtext
 
 end
 
@@ -62,13 +77,13 @@ local function do_a_checkbox(str, cvar, parent)
 end
 
 local function do_a_bunch_of_checkboxes(table, parent)
-	for k, v in pairs(table) do
-		do_a_checkbox(v, k, parent)
+	for _, v in pairs(table) do
+		do_a_checkbox(language.GetPhrase(v), v, parent)
 	end
 end
 
 local cl_cvars = {
-	scookp_cl_hide_tips = "Hide all tips",
+	"scookp_cl_hide_tips",
 }
 
 local function btnMaximDoClick(btn)
@@ -92,6 +107,22 @@ local function btnMaximDoClick(btn)
 	end
 end
 
+local function make_team_btn(parent)
+
+	parent.teamBtn = vgui.Create("DButton", parent)
+	parent.teamBtn:SetText("#scookp_menu_change_team")
+	parent.teamBtn:Dock(BOTTOM)
+	parent.teamBtn.DoClick = function(btn)
+		RunConsoleCommand("gm_showteam")
+		parent:GoHide()
+	end
+
+end
+
+concommand.Add("scookp_cl_menu", function()
+	GAMEMODE:Menu()
+end, nil, "Open the gamemode's menu")
+
 function GM:Menu()
 
 	if engine.IsPlayingDemo() then
@@ -104,22 +135,50 @@ function GM:Menu()
 	end
 
 	self.MainMenuFrame = vgui.Create("DFrame")
+
 	local frame = self.MainMenuFrame
-	local s_x, s_y = 640, 480
+
+	local s_x, s_y = 640, 400
 	frame:SetSize(s_x, s_y)
 	frame:SetPos(ScrW() / 2 - (s_x / 2), ScrH() / 2 - (s_y / 2))
+
 	frame:SetTitle(self.Name .. " menu")
+
 	frame:SetVisible(true)
 	frame:SetDraggable(true)
-	frame:ShowCloseButton(true)
 	frame:SetScreenLock(true)
+
 	frame:SetSizable(true)
 	frame:SetMinWidth(400)
 	frame:SetMinHeight(200)
+
+	frame:ShowCloseButton(true)
+
+	frame.GoHide = function(smf)
+
+		if LocalPlayer():Team() == TEAM_UNASSIGNED then
+
+			RunConsoleCommand("autoteam")
+
+			if IsValid(smf.joinBtn) then
+
+				smf.joinBtn:Remove()
+				make_team_btn(smf)
+
+			end
+
+		end
+
+		smf:ToggleVisible()
+
+	end
+
 	frame.btnMaxim:SetDisabled(false)
 	frame.btnMaxim.DoClick = function(btn) btnMaximDoClick(btn) end
+
 	frame.btnMinim:SetDisabled(false)
-	frame.btnMinim.DoClick = function(btn) frame:ToggleVisible() end
+	frame.btnMinim.DoClick = function(btn) frame:GoHide() end
+
 	frame:MakePopup()
 	frame:SetKeyboardInputEnabled(false)
 
@@ -127,14 +186,30 @@ function GM:Menu()
 	local menu = frame.sheet
 	menu:Dock(FILL)
 
-	--[[local welcome = do_tab("Welcome", "icon16/house.png", menu)
+	local welcome = do_tab("#scookp_menu_tab_welcome", "icon16/house.png", menu, true)
 
-	do_text("", welcome)]]
+	local welcome_txt = do_rich_text(welcome)
+
+	welcome_txt:InsertColorChange(16, 16, 16, 255)
+	welcome_txt:AppendText(self:GetPhrase("#scookp_menu_intro"))
+
+	if LocalPlayer():Team() == TEAM_UNASSIGNED then
+
+		frame.joinBtn = vgui.Create("DButton", frame)
+		frame.joinBtn:SetText("#scookp_menu_join_game")
+		frame.joinBtn:Dock(BOTTOM)
+		frame.joinBtn.DoClick = function(btn) frame:GoHide() end
+
+	else
+
+		make_team_btn(frame)
+
+	end
 
 
-	--[[local guide = do_tab("Guide", "icon16/book.png", menu)
+	local guide = do_tab("#scookp_menu_tab_guide", "icon16/book.png", menu)
 
-	do_text("", guide)]]
+	do_text("WIP", guide)
 
 
 	self:MenuControls(menu)
@@ -142,22 +217,26 @@ function GM:Menu()
 
 	self:MenuOptions(menu)
 
+
+	local credits = do_tab("#scookp_menu_tab_credits", "icon16/group.png", menu)
+
+	do_text("WIP", credits)
+
 end
 
 function GM:MenuControls(menu)
 
-	local controls = vgui.Create("DPanel", menu)
-	menu:AddSheet("Controls", controls, "icon16/keyboard.png")
+	local controls = do_tab("#scookp_menu_tab_controls", "icon16/keyboard.png", menu, true)
 
 	local list = vgui.Create("DListView", controls)
 	list:Dock(FILL)
 	list:SetMultiSelect(false)
-	local key = list:AddColumn("Key")
+	local key = list:AddColumn("#scookp_menu_controls_key")
 	key:SetMinWidth(40)
 	key:SetMinWidth(60)
-	local action = list:AddColumn("Options bind name")
+	local action = list:AddColumn("#scookp_menu_controls_bind_name")
 	action:SetMinWidth(100)
-	local bindname = list:AddColumn("Action in the gamemode")
+	local bindname = list:AddColumn("#scookp_menu_controls_gamemode_action")
 	bindname:SetMinWidth(100)
 
 	for _, v in pairs(binds()) do
@@ -173,7 +252,7 @@ function GM:MenuOptions(menu)
 	do_a_bunch_of_checkboxes(cl_cvars, DScrollPanel)
 
 	local vollbl = vgui.Create("DLabel", DScrollPanel)
-	vollbl:SetText("Music volume")
+	vollbl:SetText("#scookp_menu_options_music_volume")
 	vollbl:Dock(TOP)
 	vollbl:DockMargin(10, 10, 10, 0)
 	vollbl:DockPadding(10, 10, 10, 0)
