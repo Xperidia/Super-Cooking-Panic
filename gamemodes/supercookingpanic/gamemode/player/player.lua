eee8223cc31b71302a95344c427a5976dec2c925
VictorienXP <VictorienXP@Xperidia.com>
2020-08-10 11:23:17 +0200

    Prevent players from spawning on the cooking pot

--- gamemodes/supercookingpanic/gamemode/player/player.lua
+++ gamemodes/supercookingpanic/gamemode/player/player.lua
@@ -270,3 +270,44 @@ function GM:SetupPlayerVisibility(pPlayer, pViewEntity)
 	end
 
 end
+
+--[[---------------------------------------------------------
+	Name: gamemode:IsSpawnpointSuitable(player)
+	Desc: Find out if the spawnpoint is suitable or not
+-----------------------------------------------------------]]
+function GM:IsSpawnpointSuitable(ply, spawnpointent, bMakeSuitable)
+
+	local Pos = spawnpointent:GetPos()
+
+	-- Note that we're searching the default hull size here for a player in the way of our spawning.
+	-- This seems pretty rough, seeing as our player's hull could be different.. but it should do the job
+	-- (HL2DM kills everything within a 128 unit radius)
+	local Ents = ents.FindInBox( Pos + Vector(-16, -16, 0), Pos + Vector(16, 16, 64) )
+
+	if ply:Team() == TEAM_SPECTATOR then return true end
+
+	local Blockers = 0
+
+	for _, v in pairs(Ents) do
+
+		if IsValid(v) and v ~= ply and v:GetClass() == "player" and v:Alive() then
+
+			Blockers = Blockers + 1
+
+			if bMakeSuitable then
+				v:Kill()
+			end
+
+		elseif IsValid(v) and v:GetClass() == "scookp_cooking_pot" then
+
+			Blockers = Blockers + 1
+
+		end
+
+	end
+
+	if bMakeSuitable then return true end
+	if Blockers > 0 then return false end
+	return true
+
+end
