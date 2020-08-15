0a193cd61ea89dced764cb43132477607d08c645
VictorienXP <VictorienXP@Xperidia.com>
2020-08-16 00:19:13 +0200

    Update GM:CheckBind

--- gamemodes/supercookingpanic/gamemode/cl_utils.lua
+++ gamemodes/supercookingpanic/gamemode/cl_utils.lua
@@ -3,9 +3,39 @@
 				by Xperidia (2020)
 -----------------------------------------------------------]]
 
+local bind_text = {
+	MOUSE1 = "left_mouse_click",
+	MOUSE2 = "right_mouse_click",
+	MOUSE3 = "middle_mouse_click",
+}
+
+local bind_check = {
+	["+attack"] = "MOUSE1",
+	["+attack2"] = "MOUSE2",
+}
+
 function GM:CheckBind(cmd)
-	if input.LookupBinding(cmd, true) then
-		return string.upper(input.LookupBinding(cmd, true))
+
+	local bind = input.LookupBinding(cmd, true)
+
+	if bind_check[cmd] then
+
+		bind = bind_check[cmd]
+
 	end
+
+	if bind_text[bind] then
+
+		return language.GetPhrase(bind_text[bind])
+
+	end
+
+	if bind then
+
+		return string.upper(bind)
+
+	end
+
 	return "N/A"
+
 end
