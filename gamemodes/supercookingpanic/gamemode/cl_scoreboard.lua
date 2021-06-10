94dedb69a1e7a67f1432cb2c97233a5693f6dfd6
VictorienXP <victorienxp@pm.me>
2021-06-10 14:24:18 +0200

    Add the player voice chat volume setting on the scoreboard

--- gamemodes/supercookingpanic/gamemode/cl_scoreboard.lua
+++ gamemodes/supercookingpanic/gamemode/cl_scoreboard.lua
@@ -135,6 +135,16 @@ local PLAYER_LINE = {
 			end
 
 			self.Mute.DoClick = function() self.Player:SetMuted(not self.Muted) end
+			self.Mute.OnMouseWheeled = function(s, delta)
+				self.Player:SetVoiceVolumeScale(self.Player:GetVoiceVolumeScale() + (delta / 100 * 5))
+				s.LastTick = CurTime()
+			end
+
+			self.Mute.PaintOver = function(s, w, h)
+				local a = 255 - math.Clamp(CurTime() - (s.LastTick or 0), 0, 3) * 255
+				draw.RoundedBox(4, 0, 0, w, h, Color(0, 0, 0, a * 0.75))
+				draw.SimpleText(math.ceil(self.Player:GetVoiceVolumeScale() * 100) .. "%", "DermaDefaultBold", w / 2, h / 2, Color(255, 255, 255, a), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+			end
 
 		end
 
